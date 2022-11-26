eb435c982b0c7fd29c63579e29d07f66755c6215
takonomura <takonomura@users.noreply.github.com>
2022-11-27 05:45:58 +0900

    Add skeleton for passwd

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,23 @@ extern crate libc;
 extern crate lazy_static;
 #[macro_use]
 extern crate libnss;
+
+use libnss::interop::Response;
+use libnss::passwd::{Passwd, PasswdHooks};
+
+struct JsonFilePasswd;
+libnss_passwd_hooks!(jsonfile, JsonFilePasswd);
+
+impl PasswdHooks for JsonFilePasswd {
+    fn get_all_entries() -> Response<Vec<Passwd>> {
+        Response::Success(vec![])
+    }
+
+    fn get_entry_by_uid(uid: libc::uid_t) -> Response<Passwd> {
+        Response::NotFound
+    }
+
+    fn get_entry_by_name(name: String) -> Response<Passwd> {
+        Response::NotFound
+    }
+}
